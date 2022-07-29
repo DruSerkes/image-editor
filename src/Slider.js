3d8dc5f28a848291e5bd3685ff60905107180861
Dru Serkes <druserkes@gmail.com>
2022-07-28 23:18:54 -0400

    create grid with initial styles and generic image

---
+++ src/Slider.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const Slider = () => {
+  return (
+    <div className='slider-container'>
+      <input type="range" className='slider' />
+    </div>
+  )
+};
